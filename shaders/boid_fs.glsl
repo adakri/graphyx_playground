6775002fc9d7f5332003522b2bd0de8c258853da
adakri <i.casilas01@gmail.com>
2023-04-26 01:47:34 +0200

    Changes to shaders

---
+++ shaders/boid_fs.glsl
@@ -0,0 +1,10 @@
+#version 430 core
+// Standard frag shader with transparency
+in vec3 fColor;
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(fColor, 0.2);
+}
---
+++ shaders/boid_gs.glsl
@@ -0,0 +1,49 @@
+#version 330 core
+layout (triangles) in;
+layout (triangle_strip, max_vertices = 6) out;
+
+in VS_OUT {
+    vec3 color;
+} gs_in[];
+
+out vec3 fColor;
+
+/*
+Shape of boid
+        *
+       *  *
+      *    *
+     *      *
+    *        *
+   *    *     *  <----- New point
+  *  **   **   *
+ * *          * *
+
+*/
+void build_boid(vec4 position0,vec4 position1,vec4 position2)
+{    
+    fColor = gs_in[0].color; // gs_in[0] since there's one colour
+    // Barycenter
+    vec4 Barycenter = (position0+position1+position2) / 3.;
+    // First triangle    
+    gl_Position = position0; // 1:bottom-left   
+    EmitVertex();   
+    gl_Position = position2; // 2:bottom-right
+    EmitVertex();
+    gl_Position = Barycenter; // 3:top-left
+    EmitVertex();
+    EndPrimitive();
+
+    // Second triangle
+    gl_Position = position2; // 1:bottom-left   
+    EmitVertex();   
+    gl_Position = position1; // 2:bottom-right
+    EmitVertex();
+    gl_Position = Barycenter; // 3:top-left
+    EmitVertex();
+    EndPrimitive();
+}
+
+void main() {    
+    build_boid(gl_in[0].gl_Position,gl_in[1].gl_Position,gl_in[2].gl_Position);
+}
